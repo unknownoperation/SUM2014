8029deb4df34d1f9eb9a161dd862761d2bf4425e
unknown <SPR02@R111-22.(none)>
2014-06-11 18:52:04 +0400

    afadf

---
+++ T05GLOBE/VEC.C
@@ -0,0 +1,23 @@
+/* FILENAME: IMAGE.C
+ * PROGRAMMER: OK2
+ * PURPOSE: Image handle functions
+ * LAST UPDATE: 06.06.2014
+ */
+
+#include "vec.h"
+
+/* единичная матрица */
+MATR UnitMatrix =
+{
+  {
+    {1, 0, 0, 0},
+    {0, 1, 0, 0},
+    {0, 0, 1, 0},
+    {0, 0, 0, 1},
+  }
+};
+
+
+
+
+/* END OF 'VEC.C' FILE */
